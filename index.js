cb7b6c6aa041bc51c9f9653272f4b1137694e47a
vishnu110222 <purohitvishnu609@gmail.com>
2025-07-14 10:56:46 +0530

    2 program complete

---
+++ index.js
@@ -0,0 +1,23 @@
+// console.log('hello');
+
+// check even or odd
+
+// const no=7;
+
+// if(no%2==0){
+//     console.log('even');
+// }
+
+// else{
+//     console.log('odd');
+// }
+
+
+// find the maximum of 2 numbers
+
+a=5;
+b=9;
+
+if(a>b){
+    
+}
