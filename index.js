74e3844b3cf93099e45615207bb9e095564bedd5
vishnu110222 <purohitvishnu609@gmail.com>
2025-07-14 11:39:39 +0530

    4 program complete

--- index.js
+++ index.js
@@ -15,10 +15,36 @@
 
 // find the maximum of 2 numbers
 
-a=5;
-b=9;
+// a=5;
+// b=9;
 
-if(a>b){
+// if(a>b){
+//     console.log('a is large');
+// }
+
+// else{
+//     console.log('b is large');
+// }
+
+
+//check leap year
+
+// a=2020;
+// if(a%4==0){
+//         console.log('a is leap year');
+//     }
     
+// else{
+//         console.log('a is not leap year');
+// }
+
+// sum of natural numbers
+
+let a=5;
+let total=0;
+for(i=1; i<=a; i++)
+{
+    
+    total=total+i;
 }
-hello welcome
+console.log(total);
